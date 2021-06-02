88c16964a5da836a4a6451fe031c32ad1d32148a
Yaroslav Litvinov <yaroslav.litvinov@gmail.com>
2021-06-02 19:34:26 +0300

    migrate to new api

--- deploy.js
+++ deploy.js
@@ -1,3 +1,18 @@
 await hoctail.wait(() => {
-  require('pokemon').main(hoc)
+
+  function ensureTableExist (schema, name, columns) {
+    let table = schema.table(name)
+    if (!table) {
+      table = schema.addTable(name)
+      for (const name in columns) {
+	table.ensureColumn(hocTypes.Json, name, columns[name])
+      }
+    }
+    return table
+  }
+
+  
+  require('pokemon').main({
+    ensureTableExist
+  })
 })
--- index.js
+++ index.js
@@ -1,69 +1,50 @@
 const { serverSideTx, hocTypes } = require('@hoctail/patch-interface')
+const Pokedex = require('pokedex-promise-v2')
+const pokedex = new Pokedex()
 
-function ensureTableExist (schema, name, columns) {
-  let table = schema.table(name)
-  if (!table) {
-    table = schema.addTable(name)
-    for (const name in columns) {
-      table.ensureColumn(hocTypes.Json, name, columns[name])
-    }
-  }
-  return table
-}
-
-function main () {
+function main (api) {
   let table
-  console.log('main')
-  serverSideTx(hoc, store => {
+  serverSideTx(hoc, ({ store }) => {
     const { schema } = store.system
-
-    const pokemons = ensureTableExist(schema, 'pokemons', {
+    const pokemons = api.ensureTableExist(schema, 'pokemons', {
       Name: 'singleLine',
     })
-    
-    const cmds = ensureTableExist(schema, 'cmds', {
+    const cmds = api.ensureTableExist(schema, 'cmds', {
       Add: 'action',
       Delete: 'action',
       Number: 'number',
     })
-    cmds.onEvent('update of "Add"', poke_add)
-    cmds.onEvent('update of "Delete"', poke_delete)
+    cmds.column('Add').setAction(poke_add)
+    cmds.column('Delete').setAction(poke_delete)
     if (!cmds.records.size) {
       cmds.insertRecordData({ Number: 1 })
     }
-    // trigger event now
-    // const rec = Array.from(cmds.records.values())[0]
-    // rec.set('Add', new Date())
-    // console.log('updated', rec.object())
   })
 }
 
-function poke_delete (hoc, data, tx) {
-  console.log('poke_delete')
-  const number = data.new.Number
-  serverSideTx(hoc, store => {
-    const { schema } = store.system
+function poke_delete (event) {
+  event.stx(({ root }) => {
+    const number = root.triggerRecord().column('Number').value
+    const { schema } = root.system
     const pokemons = schema.table('pokemons')
     const idsToDel = Array.from(pokemons.records.keys()).slice(-number)
     idsToDel.forEach(id => pokemons.deleteRecord(id))
-  }, { id: data.schema })
+  })
 }
 
-async function poke_add (hoc, data, tx) {
-  console.log('poke_add')
-  const number = data.new.Number
-  const Pokedex = require('pokedex-promise-v2')
-  const pokedex = new Pokedex()
+async function poke_add (event) {
+  let numer, offset
+  event.stx(({ root }) => {
+    number = root.triggerRecord().column('Number').value
+    offset = root.triggerSchema().table('pokemons').records.size
+  })
 
-  const offset = hoc.sql('select count(1) from pokemons')[0].count
-  
   const list = await pokedex.getPokemonsList({
     limit: number,
     offset: offset
   })
-  serverSideTx(hoc, store => {
-    const { schema } = store.system
-    const pokemons = schema.table('pokemons')
+  event.stx(({ root }) => {
+    const pokemons = root.system.schema.table('pokemons')
     list.results.forEach(({ name, url }) => {
       console.log('add pokemon', name)
       if (!pokemons.findRecordByValues({ Name: name }) ) {
@@ -72,7 +53,7 @@ async function poke_add (hoc, data, tx) {
 	})
       }
     })
-  }, { id: data.schema })
+  })
 }
 
 module.exports = {
